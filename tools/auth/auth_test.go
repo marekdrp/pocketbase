bac7cc29ea5f01f2b880a98d19013f971b593a18
Marek Dropmann <marek@dropmann.dev>
2025-09-28 19:08:52 +0200

    Fixed auth provider test

--- tools/auth/auth_test.go
+++ tools/auth/auth_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestProvidersCount(t *testing.T) {
-	expected := 32
+	expected := 33
 
 	if total := len(auth.Providers); total != expected {
 		t.Fatalf("Expected %d providers, got %d", expected, total)
